2ee30b16c1da7fb438e20aa2484211d6bdf88118
rodrimabusiness <rodrimabusiness@gmail.com>
2024-05-15 15:55:46 +0100

    reverted and functional

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -2,8 +2,8 @@
 
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { CheerioAPI } from "cheerio";
 import { extractCurrency, extractPrice, getSelectors } from "../utils";
-import https from "https";
 
 export async function scrapeAnyProduct(url: string): Promise<any> {
   if (!url) return null;
@@ -14,53 +14,27 @@ export async function scrapeAnyProduct(url: string): Promise<any> {
   const port = 22225;
   const session_id = Math.floor(Math.random() * 1000000);
 
-  const agent = new https.Agent({
-    rejectUnauthorized: false,
-  });
-
   const options = {
-    httpsAgent: agent,
-    proxy: {
-      host: "brd.superproxy.io",
-      port: port,
-      auth: {
-        username: `${username}-session-${session_id}`,
-        password: password,
-      },
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
     },
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-      "Accept-Language": "en-US,en;q=0.9",
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache",
-      Pragma: "no-cache",
-    },
-    timeout: 10000, // Adicionando timeout de 10 segundos
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false, // Importante para evitar problemas com certificados
   };
 
   try {
-    console.log("Fetching data from URL:", url);
-    console.log("Using session ID:", session_id);
-
-    const startTime = Date.now();
-
     // Tentativa de obtenção da página
     const response = await axios.get(url, options);
-    const fetchDuration = Date.now() - startTime;
-    console.log(`Data fetched successfully in ${fetchDuration}ms`);
-
     if (!response || !response.data) {
       console.error("Failed to fetch data or no data in the response.");
       return null;
     }
-
-    const parseStartTime = Date.now();
     const $ = cheerio.load(response.data);
     const selectors = getSelectors(url);
 
+    // Extração de dados utilizando os seletores definidos
     const title = $(selectors.titleSelector).text().trim();
 
     const { currentPrice, recommendedPrice } = extractPrice(
@@ -94,22 +68,11 @@ export async function scrapeAnyProduct(url: string): Promise<any> {
       averagePrice: currentPrice,
     };
 
-    const parseDuration = Date.now() - parseStartTime;
-    console.log(`Data parsed successfully in ${parseDuration}ms`);
-
     console.log("Product Data:", data);
     return data;
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
+  } catch (error: unknown) {
+    if (error instanceof Error) {
       console.error("Error during product scraping:", error.message);
-      if (error.response) {
-        console.error("Response status:", error.response.status);
-        console.error(
-          "Response headers:",
-          JSON.stringify(error.response.headers, null, 2)
-        );
-        console.error("Response data:", error.response.data);
-      }
     } else {
       console.error(
         "Error during product scraping: An unknown error occurred."
